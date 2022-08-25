63610be4c4fed97ea8c9e20e296b76954752f80d
jayog18 <93071209+jayog18@users.noreply.github.com>
2022-08-25 21:53:20 +0530

    Create ps4.py

---
+++ ps4.py
@@ -0,0 +1,7 @@
+a=int(input("enter your num  : "))
+x=a%10
+if (x%3==0):
+  print("last digit of ",a,"is divisible by 3")
+else:
+  print("last digit of ",a,"is not divisible by 3")
+#print("last digit is",x)
