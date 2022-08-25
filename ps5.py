f6e8e00a996f6e2d20b036b37c5e73fea1be91eb
jayog18 <93071209+jayog18@users.noreply.github.com>
2022-08-25 22:26:28 +0530

    Create ps5.py

---
+++ ps5.py
@@ -0,0 +1,9 @@
+m=float(input("enter your marks : "))
+#while m>0:
+  if m>90 :
+    print("your grade is A")
+    continue
+  if 80<m<90 :
+    print("your grade is B")
+  if 70<m<80 :
+    print("your grade is c")
