321ee1c7473d5e5b6af95056e5805333d156a1a6
jayog18 <93071209+jayog18@users.noreply.github.com>
2022-08-25 23:47:03 +0530

    Update ps5.py

--- ps5.py
+++ ps5.py
@@ -7,3 +7,16 @@ m=float(input("enter your marks : "))
     print("your grade is B")
   if 70<m<80 :
     print("your grade is c")
+    
+    
+while (True):
+  m=float(input("enter your marks : "))
+  if m>90:
+    print("your grade is A")  
+  if 80<m<90 :
+    print("your grade is B")
+  if 70<m<80 :
+    print("your grade is c")
+  if 60<m<70:
+    print("your grade is D")    
+    
